50860d5e0b9809ad6e10fef22fea8154504ed476
Barkat Ullah <barkatullah.zx@gmail.com>
2023-04-01 05:57:41 +0600

    Design & Data Dynamic

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -22,6 +22,23 @@ const Blogs = () => {
             <div className='bg-purple-100 rounded-lg text-center p-4 outline-purple-600 outline'>
                 <h2 className='text-lg font-semibold text-purple-600'>Spent time on read : 0 min</h2>
             </div>
+    
+            <div className="overflow-x-auto mt-5">
+  <table className="table w-full rounded-2xl">
+  <thead>
+      <tr>
+        <th className='text-center text-lg font-bold'>Bookmark Blogs:</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      <tr>
+        <td className='text-xl font-semibold'>How to get your first job as a self-taught programmer
+</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
             </div>
         </div>
     );
